d6cce3efb3a33dff2c5d94d525376eff0c08b30b
Peter <Peterlee2094@gmail.com>
2023-10-10 12:55:01 -0500

    removed boilerplate code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
 function App() {
-  const [count, setCount] = useState(0);
-
   const testFetch = async () => {
     axios.get("/api/v1/").then(async (response) => {
       console.log(response.data);
@@ -13,24 +8,9 @@ function App() {
   };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={testFetch}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={testFetch}>click</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
