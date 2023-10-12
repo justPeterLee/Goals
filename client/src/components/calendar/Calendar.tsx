3c6fee402a6c08d6bab2aa348b5b0718519599be
Peter <Peterlee2094@gmail.com>
2023-10-11 22:01:46 -0500

    create calendar component

---
+++ client/src/components/calendar/Calendar.tsx
@@ -0,0 +1,14 @@
+export default function Calendar() {
+  return (
+    <div>
+      <Week />
+      <Week />
+      <Week />
+      <Week />
+    </div>
+  );
+}
+
+function Week() {
+  return <div>week</div>;
+}
