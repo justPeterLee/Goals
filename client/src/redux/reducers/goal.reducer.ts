40c1dae0e231dc0097556871ecaab3fdebc5d732
Peter <Peterlee2094@gmail.com>
2023-10-28 14:17:20 -0500

    created redux route for task

--- client/src/redux/reducers/goal.reducer.ts
+++ client/src/redux/reducers/goal.reducer.ts
@@ -1,6 +1,10 @@
 // import { UnknownAction } from "redux";
 import { combineReducers } from "@reduxjs/toolkit";
 
+interface action {
+  type: string;
+  payload: any;
+}
 function goals(state: any = [], action: { type: string; payload: any }) {
   switch (action.type) {
     case "SET_GOALS":
@@ -13,6 +17,19 @@ function goals(state: any = [], action: { type: string; payload: any }) {
   }
 }
 
+function task(state: any = [], action: action) {
+  switch (action.type) {
+    case "SET_TASK":
+      if (action.payload) {
+        console.log("redux test");
+        return action.payload;
+      }
+      return state;
+    default:
+      return state;
+  }
+}
 export default combineReducers({
   goals,
+  task,
 });
